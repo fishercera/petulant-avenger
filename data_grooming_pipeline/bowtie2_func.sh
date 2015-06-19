e67e59b88ba9daab7da5a5d7c38aebb162d8aa8e
Cera Fisher <cera.fisher@uconn.edu>
2015-06-19 19:22:27 -0400

    Added my UCDavis scripts to utilities, and started writing the bits and pieces of the pipelines as separate functionst

--- data_grooming_pipeline/bowtie2_func.sh
+++ data_grooming_pipeline/bowtie2_func.sh
@@ -2,20 +2,27 @@
 
 # This function filters out the contaminants from 
 # quality trimmed reads. 
+
 # USAGE: filterContams <lib-base-name> <index-name> <path/to/index>
 # 
 # function filterContams {
-# lib=$1
+# lib=$1 
 # index=$2
 # indexPath=$3
 # }
-function filterContams {
+function filterContamsPE {
 lib=$1
 index=$2
 indexPath=$3
 
-bowtie2 -q --phred33 --mm --very-fast -k 1 -I 250 -X 1000 --dovetail --met-file bowtie2Metrics-rRNA.out --un $lib.Unpaired%.filtered.fastq --al $lib.Unpaired%.contams.fastq --un-conc $lib.P%.filtered.fastq --al-conc $lib.P%.contams.fastq -x $indexPath -1 $lib.P1.step3.fastq -2 $lib.P2.step3.fastq -S $lib.paired.$index.sam
+echo "Starting bowtie2 with parameters $lib, $index, $indexPath"
+
+bowtie2 -q --phred33 --mm --very-fast -k 1 -I 250 -X 1000 --dovetail --met-file bowtie2Metrics-rRNA.out --un $lib.Unpaired%.filtered.fastq --al $lib.Unpaired%.contams.fastq --un-conc $lib.P%.filtered.fastq --al-conc $lib.P%.contams.fastq -x $indexPath -1 $lib.P1.step3.fastq -2 $lib.P2.step3.fastq -S $lib.paired.$index.sam > $lib.$index.log
+
+cat $lib.$index.log
+
 }
 
 # filterContams tinytest hg19 "/apps/bowtie2/1.0.0/indexes/hg19"
 # Output of this function will be the output of the bowtie2 command
+
