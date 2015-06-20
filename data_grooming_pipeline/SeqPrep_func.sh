47ef32197d807e730450fc48a1588efbc99f0c98
Cera Fisher <cera.fisher@uconn.edu>
2015-06-20 15:38:29 -0400

    Updating SP function

--- data_grooming_pipeline/SeqPrep_func.sh
+++ data_grooming_pipeline/SeqPrep_func.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/bash
+
+
+#SeqPrep -f $lib.P1.filtered.fastq -r $lib.P2.filtered.fastq -1 $lib.P1.SP.fastq -2 $lib.P2.SP.fastq -s $lib.merged.SP.fastq -g -E $lib.alignments.fasta -A -B 
+#USAGE: sp <lib-base-name>
+#function sp {
+# lib = $1
+# 
+# SeqPrep -f $lib.P1.filtered.fastq -r $lib.P2.filtered.fastq -1 $lib.P1.SP.fastq -2 $lib.P2.SP.fastq -s $lib.merged.SP.fastq -g -E $lib.alignments.fasta -A -B 
+#}
+# TODO: add functionality to accept adapter strings
+function sp {
+  lib = $1
+  
+  echo "Performing SeqPrep on $lib.P1.filtered.fastq and $lib.P2.filtered.fastq"
+  
+  SeqPrep -f $lib.P1.filtered.fastq -r $lib.P2.filtered.fastq -1 $lib.P1.SP.fastq -2 $lib.P2.SP.fastq -s $lib.merged.SP.fastq -g -E $lib.alignments.fasta -A -B > $lib.sp.log
+  cat $lib.sp.log
+  
+}
---
+++ data_grooming_pipeline/grepAdapters_TODO.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#Sliding window to get n-char long snips of a string (to use to grep for adapters)
+# ${#string} outputs the length of the string
+# ${string:position} gets a substring
+#    ie ${string:0:20} gives first twenty chars of a string
+# I should just write this in perl, it would not be hard at all. 
+
