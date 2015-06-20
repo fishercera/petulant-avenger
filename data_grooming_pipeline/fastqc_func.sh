cab8582572d77aef50963f74883c08fbde7f0884
Cera Fisher <cera.fisher@uconn.edu>
2015-06-20 15:43:09 -0400

    added last fastqc function

--- data_grooming_pipeline/fastqc_func.sh
+++ data_grooming_pipeline/fastqc_func.sh
@@ -11,5 +11,16 @@ function fastqcB4 {
 lib=$1
 echo "Running fastqc on $lib.*.fastq.gz"
 
-fastqc -o output/fastqcBEFORE $lib.*.fastq.gz
+fastqc -o output/fastqcBEFORE input/$lib.*.fastq.gz
+}
+
+# USAGE: fastqc <lib-base-name> 
+# function fastqcAFTER {
+# 
+# fastqc -o output/fastqcAFTER $lib.*.fastq.gz
+# }
+function fastqcAFTER {
+  echo "Running fastqc on $lib files"
+  
+  fastqc -o output/fastqcAFTER output/$lib.*.fastq.gz
 }
