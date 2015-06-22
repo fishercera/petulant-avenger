9f429f35f1d1a5f1a482b85f96702b963dffa7b3
Cera Fisher <cera.fisher@uconn.edu>
2015-06-22 12:37:01 -0400

    Updated bowtie functions

--- data_grooming_pipeline/bowtie2_func.sh
+++ data_grooming_pipeline/bowtie2_func.sh
@@ -10,19 +10,70 @@
 # index=$2
 # indexPath=$3
 # }
+
+
+
+
 function filterContamsPE {
-lib=$1
+pass=$1
 index=$2
 indexPath=$3
+libbase=$4
 
-echo "Starting bowtie2 with parameters $lib, $index, $indexPath"
+echo "Starting bowtie2 with parameters $pass, $index, $indexPath, $libbase"
 
-bowtie2 -q --phred33 --mm --very-fast -k 1 -I 250 -X 1000 --dovetail --met-file bowtie2Metrics-rRNA.out --un $lib.Unpaired%.filtered.fastq --al $lib.Unpaired%.contams.fastq --un-conc $lib.P%.filtered.fastq --al-conc $lib.P%.contams.fastq -x $indexPath -1 $lib.P1.step3.fastq -2 $lib.P2.step3.fastq -S $lib.paired.$index.sam > $lib.$index.log
-
-cat $lib.$index.log
+echo "bowtie2 -q --phred33 --mm --very-fast -k 1 -I 250 -X 1000 --dovetail --met-file bowtie2Metrics-$index.out --un $libbase$index.Unpaired.filtered.fastq --al $libbase$index.Unpaired.contams.fastq --un-conc $libbase$index.P%.filtered.fastq --al-conc $libbase$index.P%.contams.fastq -x $indexPath/$index -1 $pass.P1.filtered.fastq -2 $pass.P2.filtered.fastq -S $pass.paired.$index.sam "
+echo "### The outputs become the inputs: "
+echo "### $libbase$index.Unpaired.filtered.fastq and "
+echo "### $libbase$index.P%.filtered.fastq ###"
 
 }
 
 # filterContams tinytest hg19 "/apps/bowtie2/1.0.0/indexes/hg19"
 # Output of this function will be the output of the bowtie2 command
+
+function filterContamsSE {
+  pass=$1
+  index=$2
+  indexPath=$3
+  libbase=$4
+
+  echo "Starting bowtie2 with parameters $pass, $index, $indexPath, $libbase"
+
+  echo "bowtie2 -q --phred33 --mm --very-fast -k 1 -I 250 -X 1000 --met-file bowtie2Metrics-rRNA.out --un input/scratch/$libbase$index.Unpaired.filtered.fastq --al input/scratch/$libbase$index.Unpaired.contams.fastq -U input/scratch/$pass.Unpaired.filtered.fastq -x $indexPath/$index -S input/scratch/$pass.unpaired.$index.sam" 
+
+
+
+}
+
+args="$@"
+numargs=$#
+echo $args
+echo $numargs
+echo "libbase = $1"
+libbase=$1
+shift
+echo "indexPath = $1"
+indexPath=$1
+shift
+echo "indices = $@"
+indices=$@
+echo $indices
+pass=$libbase
+for i in $indices
+
+do
+  echo $i
+  echo "----"
+  filterContamsPE $pass $i $indexPath $libbase
+  filterContamsSE $pass $i $indexPath $libbase
+  pass=$libbase$i
+  echo "-----"
+  echo $pass
+done
+
+
+
+# filterContamsPE $libbase $index $indexPath
+# filterContamsSE $libbase $index $indexPath
 
