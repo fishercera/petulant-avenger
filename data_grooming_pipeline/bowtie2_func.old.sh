37a6a770c188645af807bf3710dc6f75b2f2a747
Cera Fisher <cera.fisher@uconn.edu>
2015-06-27 18:34:30 -0400

    Fixed the bowtie functions

--- data_grooming_pipeline/bowtie2_func.old.sh
+++ data_grooming_pipeline/bowtie2_func.old.sh
@@ -4,6 +4,8 @@
 # USAGE: bowtie2_func.sh <lib-base-name> <path-to-indices> <List that is a name of indices>
 # For example: bowtie2_func.sh ELJ1393 ~/mini/bt2 Greg_rRNA dmelan scerev
 
+
+
 # filterContamsPE - this works on paired reads and filters out the pairs that align concordantly. We're being very generous
 # with the definition of concordant, allowing dovetailed reads just in case we get readthrough. 
 #USAGE: filterContamsPE <lib-base-name> <index> <index-path>
@@ -13,16 +15,19 @@ index=$2 # The index base, ie Greg_rRNA
 indexPath=$3 # The path to the index, ie ~/mini/bt2
 
 echo "Starting bowtie2 with parameters $lib, $index, $indexPath"
-un=input/scratch/$lib$index.U.filtered.fastq
-al=input/scratch/$lib$index.U.contams.fastq
-unconc=input/scratch/$lib$index.P%.filtered.fastq
-alconc=input/scratch/$lib$index.P%.contams.fastq
-bowtie2 -q --phred33  -p 8 --very-sensitive  --dovetail --met-file bowtie2Metrics-$index.out --un $un --al $al --al-conc $alconc --un-conc $unconc -x $indexPath/$index -1 input/$lib.R1.fastq -2 input/$lib.R2.fastq -S deleteme$lib.paired.$index.sam 
 
-      cp input/$lib.R1.fastq input/scratch/$lib.R1.fastq_pre$index
-      cp input/$lib.R2.fastq input/scratch/$lib.R2.fastq_pre$index
-      cp input/scratch/$lib$index.P1.filtered.fastq input/$lib.R1.fastq
-      cp input/scratch/$lib$index.P2.filtered.fastq input/$lib.R2.fastq
+bowtie2 -q --phred33 --mm --very-sensitive -I 250 -X 1000 --dovetail --met-file bowtie2Metrics-$index.out --un input/scratch/$lib$index.U.filtered.fastq \
+      --al input/scratch/$lib$index.U.contams.fastq --un-conc input/scratch/$lib$index.P%.filtered.fastq --al-conc input/scratch/$lib$index.P%.contams.fastq \
+      -x $indexPath/$index -1 \
+      input/$lib.R1.fastq -2 input/$lib.R2.fastq -S deleteme$lib.paired.$index.sam 
+#       touch input/scratch/$lib$index.P1.filtered.fastq
+#       touch input/scratch/$lib$index.P2.filtered.fastq
+#       touch input/scratch/$lib$index.P1.contams.fastq
+#       touch input/scratch/$lib$index.P2.contams.fastq
+      mv input/$lib.R1.fastq input/scratch/$lib.R1.fastq_pre$index
+      mv input/$lib.R2.fastq input/scratch/$lib.R2.fastq_pre$index
+      mv input/scratch/$lib$index.P1.filtered.fastq input/$lib.R1.fastq
+      mv input/scratch/$lib$index.P2.filtered.fastq input/$lib.R2.fastq
 
 # These lines will ensure that the next run through this is going to use the filtered files, and we'll keep filtering stuff out. 
 # This seems to be ready to go.. need to just uncomment it and try it out with th genomes I've got on minidrive
@@ -45,13 +50,11 @@ function filterContamsSE {
   lib=$1
   index=$2
   indexPath=$3
-  input=input/scratch/$lib.U.filtered.fastq
-  al=input/scratch/$lib$index.U.contams.fastq
-  un=input/scratch/$lib$index.U.filtered.fastq
 # Input files need to be in the form <lib-base-name>.U1/2.filtered.fastq 
   echo "Starting bowtie2 with parameters $lib, $index, $indexPath"
-  bowtie2 -q --phred33 -p 8 --very-sensitive --met-file bowtie2SEMetrics-$index.out --un $un --al $al -x $indexPath/$index -U $input -S deleteme$lib$index.SE.sam
-  cp $un $input 
+  cp input/$lib.U.trimmed.fastq input/scratch/$lib.U.filtered.fastq
+  bowtie2 -q --phred33 -p 4 --very-sensitive --met-file bowtie2SEMetrics-$index.out --un input/scratch/$lib$index.U.filtered.fastq --al input/scratch/$lib$index.U.contams.fastq -x $indexPath/$index -U input/scratch/$lib.U.filtered.fastq -S deleteme.sam
+  mv input/scratch/$lib$index.U.filtered.fastq input/scratch/$lib.U.filtered.fastq
 
   touch Unpairedbowtie$index.done
 }
@@ -81,7 +84,6 @@ do
   echo $lib 
 done
 
-cp input/$lib.U.trimmed.fastq input/scratch/$lib.U.filtered.fastq
 for i in $indices
 do
   echo $i
@@ -90,8 +92,4 @@ do
   echo "-----"
   echo $lib
 done
-
-cp input/scratch/$lib.U.filtered.fastq output/.
-cp input/$lib.R1.fastq output/.
-cp input/$lib.R2.fastq output/.
 
